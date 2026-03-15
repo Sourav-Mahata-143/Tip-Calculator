8a647092b7b2f419365887c7e9ceb43516394bdd
Sourav Mahata <117214592+Sourav-Mahata-143@users.noreply.github.com>
2026-03-15 21:09:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("calculate");
+const billInput = document.getElementById("bill");
+const tipInput = document.getElementById("tip");
+const totalSpan = document.getElementById("total");
+
+function calculateTotal(){
+  const billValue = Number(billInput.value);
+  const tipValue = Number(tipInput.value);
+  const totalValue = billValue * (1 + tipValue/100);
+  totalSpan.innerText = totalValue.toFixed(2);
+}
+
+btn.addEventListener("click", calculateTotal);
